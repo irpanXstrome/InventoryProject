f5bdececf842608908eaa5cc0c4d1ec87c5b2dbf
Haris Oche <oche.0325@gmail.com>
2024-04-14 11:16:15 +0700

    add is_admin in users (#2)
    Co-authored-by: oche <haris.gumilar@sekolahmu.co.id>

--- database/migrations/2024_04_07_090944_user_table.php
+++ database/migrations/2024_04_07_090944_user_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string("email")->unique();
             $table->string("username")->unique();
             $table->string("password");
+            $table->boolean('is_admin')->nullable()->default(false);
             $table->timestamps();
         });
     }
